33655a4aa67608d69d87ad2f56c85ab000b16188
cm <chanmi125128@gmail.com>
2024-07-01 14:50:25 +0900

    first commit

---
+++ src/main/java/com/project/securelogin/SecureLoginApplication.java
@@ -0,0 +1,13 @@
+package com.project.securelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureLoginApplication.class, args);
+    }
+
+}
